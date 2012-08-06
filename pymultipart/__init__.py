38d1997c93c587afc38b9e196ed5899418c4d676
Adam Venturella <aventurella@gmail.com>
2012-08-06 12:39:51 -0700

    __init__ info

--- pymultipart/__init__.py
+++ pymultipart/__init__.py
@@ -16,9 +16,8 @@
 
 __title__ = 'pymultipart'
 __version__ = '0.1'
-__build__ = 0x001306
 __author__ = 'Adam Venturella'
-__license__ = 'ISC'
+__license__ = 'Apache 2'
 __copyright__ = 'Copyright 2012 Adam Venturella'
 
 from .multipart import MultipartParser
