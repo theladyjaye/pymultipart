9b3f03b853fa92806b55053b50300e272c483acc
Adam Venturella <aventurella@gmail.com>
2012-08-05 20:09:06 -0700

    tested against python3

--- multipart.py
+++ multipart.py
@@ -1,13 +1,13 @@
 # HTTP Multipart Body Parser
 #
 # Copyright 2011 Adam Venturella
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,148 +18,142 @@
 import collections
 from tempfile import TemporaryFile
 
+
 class MultipartException(Exception):
 
     def __init__(self, message):
         self.message = message
 
+
 class MultipartParser(object):
-    
+
     def __init__(self, boundary, data):
-        self.files                  = {}
-        self.params                 = {}
-        self.current_headers        = {}
-        self.master_boundary        = "--" + boundary
+        self.files = {}
+        self.params = {}
+        self.current_headers = {}
+        self.master_boundary = "--" + boundary
         self.master_boundary_length = len(boundary) + 2
         self.start_processing(data)
 
     def start_processing(self, data):
         self.validate_master_boundary(data)
-    
+
     def end_processing(self):
         pass
 
     def validate_master_boundary(self, data):
         candidate = self._readboundary(data)
-        
-        if candidate[0] == self.master_boundary:
-            if candidate[1] == True:
+        if candidate == self.master_boundary:
+            if data.read(2) == b'--':
                 self.end_processing()
             else:
                 self.current_headers = {}
-                self.read_headers(data)
+                self.read_header(data)
         else:
             raise MultipartException("Invalid Boundary")
-            
-
 
     def read_boundry(self, data):
-        candidate = self._readboundary(data)
-        
-        if candidate[1] == True:
+        self._readboundary(data)
+
+        if data.read(2) == b'--':
             self.end_processing()
         else:
             self.current_headers = {}
-            self.read_headers(data)
+            self.read_header(data)
 
+    def start_body(self, data):
+        disposition = self.current_headers["content-disposition"]
+        if "filename" in disposition:
+            self.read_file(data)
+        else:
+            self.read_body(data)
 
-    def read_headers(self, data):
-        bytes = data.read(2)
+    def read_header(self, data):
+        line = data.readline().decode()
+        if line != '\r\n':
+            line = line.split(":")
+            key = line[0].lower().strip()
+            value = line[1].strip()
 
-        if bytes == "\r\n":
-            
-            disposition = self.current_headers["content-disposition"]
-            if "filename" in disposition:
-                self.read_file(data)
+            self.current_headers[key] = None
+
+            if key == "content-disposition":
+                self.current_headers[key] = {}
+                parts = collections.deque(value.split(";"))
+                disposition_type = parts.popleft()
+
+                if disposition_type != "form-data":
+                    # RFC 2388 requires the disposition type be "form-data"
+                    # anything else is a no op and we should skip this segment/part
+                    # http://tools.ietf.org/html/rfc2388
+                    raise MultipartException("Invalid Content-Disposition, found {0} expected form-data".format(disposition_type))
+
+                while 1:
+                    try:
+                        value = parts.popleft().strip().replace("\"", "")
+                        k, v = value.split("=")
+                        self.current_headers[key][k] = v
+                    except IndexError:
+                        break
             else:
-                self.read_body(data)
+                self.current_headers[key] = value
+
+            self.read_header(data)
         else:
-            self.read_header_item(data, bytes)
-    
-    def read_header_item(self, data, prefix=""):
-        line = prefix + data.readline()
-        
-        line  = line.split(":")
-        key   = line[0].lower().strip()
-        value = line[1].strip()
+            self.start_body(data)
 
-        self.current_headers[key] = None
-
-        if key == "content-disposition":
-            self.current_headers[key] = {}
-            parts = collections.deque(value.split(";"))
-            disposition_type = parts.popleft()
-
-            if disposition_type != "form-data":
-                # RFC 2388 requires the disposition type be "form-data"
-                # anything else is a no op and we should skip this segment/part
-                # http://tools.ietf.org/html/rfc2388
-                raise MultipartException("Invalid Content-Disposition, found {0} expected form-data".format(disposition_type))
-
-            while 1:
-                try:
-                    value = parts.popleft().strip().replace("\"", "")
-                    k,v = value.split("=")
-                    self.current_headers[key][k] = v
-                except IndexError:
-                    break;
-        else:
-            self.current_headers[key] = value
-        
-        self.read_headers(data)
-    
     def read_file(self, data):
-        temp_file = TemporaryFile(mode="w+b")
+        temp_file = TemporaryFile()
 
         if "content-length" in self.current_headers:
             temp_file.write(data.read(self.current_headers["content-length"]))
         else:
             bytes = data.readline()
-
-            while not bytes[-2:] == "\r\n":
+            while 1:
+                try:
+                    if bytes[:2].decode() == '--':
+                        break
+                except UnicodeDecodeError:
+                    pass
                 temp_file.write(bytes)
                 bytes = data.readline()
-            
-            temp_file.write(bytes.rstrip())
-        
-        filesize     = temp_file.tell()
 
+        filesize = temp_file.tell()
         if filesize == 0:
             self.read_boundry(data)
             return
-
-        key          = self.current_headers["content-disposition"]["name"]
-        filename     = self.current_headers["content-disposition"].get("filename", "")
+        key = self.current_headers["content-disposition"]["name"]
+        filename = self.current_headers["content-disposition"].get("filename", "")
         content_type = self.current_headers["content-type"]
-        
+
         if key not in self.files:
             self.files[key] = []
 
         temp_file.seek(0)
-        self.files[key].append({"filename":filename, "filesize":filesize, "content-type":content_type, "data":temp_file})
-        
-        self.read_boundry(data)
+        self.files[key].append({"filename": filename,
+                                "filesize": filesize,
+                                "content-type": content_type,
+                                "data": temp_file})
+        self.read_header(data)
 
     def read_body(self, data):
-        bytes = data.readline()
         value = ""
-        
+        bytes = data.readline().decode()
+
         while not bytes[-2:] == "\r\n":
             value = value + bytes
-            bytes = data.readline()
-        
+            bytes = data.readline().decode()
+
         value = value + bytes.rstrip()
 
-        key   = self.current_headers["content-disposition"]["name"]
+        key = self.current_headers["content-disposition"]["name"]
 
         if key not in self.params:
                 self.params[key] = []
-        
+
         self.params[key].append(value)
         self.read_boundry(data)
-    
+
     def _readboundary(self, data):
-        boundary = data.read(self.master_boundary_length)
-        is_end = True if data.read(2) == "--" else False
-        
-        return (boundary, is_end)
+        boundary = data.read(self.master_boundary_length).decode()
+        return boundary
--- test.py
+++ test.py
@@ -3,24 +3,28 @@ import time
 from multipart import MultipartParser
 
 def main():
-    filename = os.getcwd() + "/data-multipart-form-data-chrome.txt"
+    filename = os.getcwd() + "/data-multipart-wireshark.txt"
     filesize = os.path.getsize(filename)
-    
-    with open(filename) as f:
-        
-        content_type = "multipart/form-data; boundary=------WebKitFormBoundaryy68h9UzzE0zpkUU7"
+
+    with open(filename, 'rb') as f:
+
+        content_type = "multipart/form-data; boundary=----WebKitFormBoundaryeqOqmwCCwUpAZEDi"
         index        = content_type.rfind("=") + 1
         boundary     = content_type[index:]
-        
+
         start        = time.clock() * 1000
         machine      = MultipartParser(boundary, f)
-        
+
         print("\nElapsed time: " + str(time.clock() * 1000 - start) + "ms")
-        
+
         print("\n----------- Files:")
         print(machine.files )
         print("\n----------- Params:")
         print(machine.params)
+
+        with open('test.jpg', 'w+b') as f:
+            f.write(machine.files['image1'][0]['data'].read())
+        
 
 if __name__ == "__main__":
     main()
